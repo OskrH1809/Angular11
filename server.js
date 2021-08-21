a54e88fa77cc1b4794e0367eaac411f8127ea89d
OskrH1809 <48338394+OskrH1809@users.noreply.github.com>
2021-08-20 23:40:10 -0600

    configuracion server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/servicio-soporte'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/proyecto1'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/servicio-soporte/'}),
-);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/proyecto1/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
+
+
